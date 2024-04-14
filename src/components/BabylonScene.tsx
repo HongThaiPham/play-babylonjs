a813b1f40594951982dd747e03b4ce2b6ab415b8
HongThaiPham <hongthaipro@gmail.com>
2024-04-14 22:31:05 +0700

    Update BabylonScene.tsx and PhysicProvider.tsx to enable physics in the scene

--- src/components/BabylonScene.tsx
+++ src/components/BabylonScene.tsx
@@ -1,15 +1,17 @@
 "use client";
-import { Scene, Engine } from "react-babylonjs";
+import { Scene, Engine, SceneEventArgs } from "react-babylonjs";
 import "@babylonjs/core/Physics/physicsEngineComponent"; // side-effect adds scene.enablePhysics function
 import { PropsWithChildren, useEffect, useState } from "react";
-import { PhysicsShapeType, Vector3 } from "@babylonjs/core";
+import { HavokPlugin, PhysicsShapeType, Vector3 } from "@babylonjs/core";
 import "@babylonjs/loaders/glTF";
 import ShadowBox from "./ShadowBox";
 import PhysicProvider from "./PhysicProvider";
 import Map from "./Map";
 import Hero from "./Hero";
+import HavokPhysics from "@babylonjs/havok";
 const BabylonScene: React.FC<PropsWithChildren> = ({ children }) => {
   const [isClient, setIsClient] = useState(false);
+  const [physicEnabled, setPhysicEnabled] = useState(false);
   useEffect(() => {
     if (typeof window !== "undefined") {
       require("@babylonjs/core/Debug/debugLayer");
@@ -20,15 +22,40 @@ const BabylonScene: React.FC<PropsWithChildren> = ({ children }) => {
     }
   }, []);
 
+  const handleScreenMount = async (sceneEventArgs: SceneEventArgs) => {
+    console.log("onSceneMount: ", sceneEventArgs);
+    const { scene } = sceneEventArgs;
+    const engine = scene.getEngine();
+    if (scene && engine) {
+      // scene.enablePhysics(new Vector3(0, -9.81, 0), new CannonJSPlugin());
+      const _res = scene.enablePhysics(
+        new Vector3(0, -9.81, 0),
+        new HavokPlugin(false, await HavokPhysics())
+      );
+      setPhysicEnabled(_res ?? false);
+      scene.debugLayer.show();
+    }
+  };
+
   if (!isClient) {
     return null;
   }
 
   return (
-    <Engine antialias adaptToDeviceRatio canvasId="play-babylon">
-      <Scene>
-        <PhysicProvider>
-          {/* <ground
+    <Engine
+      antialias
+      adaptToDeviceRatio
+      canvasId="play-babylon"
+      style={{ height: "100vh" }}
+      engineOptions={{
+        deterministicLockstep: true,
+        lockstepMaxSteps: 4,
+      }}
+    >
+      <Scene collisionsEnabled={true} onSceneMount={handleScreenMount}>
+        {physicEnabled ? (
+          <PhysicProvider>
+            {/* <ground
             name="ground"
             width={20}
             height={15}
@@ -42,11 +69,12 @@ const BabylonScene: React.FC<PropsWithChildren> = ({ children }) => {
               _options={{ mass: 0, restitution: 0.1 }}
             />
           </ground> */}
-          <Map />
-          <ShadowBox>
-            <Hero />
-          </ShadowBox>
-        </PhysicProvider>
+            <Map />
+            <ShadowBox>
+              <Hero />
+            </ShadowBox>
+          </PhysicProvider>
+        ) : null}
       </Scene>
     </Engine>
   );
--- src/components/PhysicProvider.tsx
+++ src/components/PhysicProvider.tsx
@@ -6,26 +6,26 @@ import { useScene } from "react-babylonjs";
 import Environment from "./Environment";
 
 const PhysicProvider: React.FC<PropsWithChildren> = ({ children }) => {
-  const scene = useScene();
-  const [isEnable, setIsEnable] = useState(false);
+  // const scene = useScene();
+  // const [isEnable, setIsEnable] = useState(false);
 
-  useEffect(() => {
-    if (!scene) return;
-    window.scene = scene;
-    const engine = scene.getEngine();
-    engine.displayLoadingUI();
-    HavokPhysics().then((HK) => {
-      const isEnable = scene.enablePhysics(
-        new Vector3(0, -9.81, 0),
-        new HavokPlugin(false, HK)
-      );
+  // useEffect(() => {
+  //   if (!scene) return;
+  //   window.scene = scene;
+  //   const engine = scene.getEngine();
+  //   engine.displayLoadingUI();
+  //   HavokPhysics().then((HK) => {
+  //     const isEnable = scene.enablePhysics(
+  //       new Vector3(0, -9.81, 0),
+  //       new HavokPlugin(false, HK)
+  //     );
 
-      setIsEnable(isEnable ?? false);
-      engine.hideLoadingUI();
-    });
-  }, [scene, setIsEnable]);
+  //     setIsEnable(isEnable ?? false);
+  //     engine.hideLoadingUI();
+  //   });
+  // }, [scene, setIsEnable]);
 
-  if (!isEnable) return null;
+  // if (!isEnable) return null;
   return (
     <>
       <Environment />
