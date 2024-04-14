96f9862fae0e10823ec03181e601920d1028452b
HongThaiPham <hongthaipro@gmail.com>
2024-04-14 23:02:34 +0700

    Update Hero component to load model from local assets folder

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,12 +1,44 @@
 "use client";
 
-import { PhysicsShapeType, Quaternion, Vector3 } from "@babylonjs/core";
+import {
+  AbstractMesh,
+  PhysicsShapeType,
+  Quaternion,
+  Vector3,
+} from "@babylonjs/core";
 import { Suspense, useState } from "react";
-import { Model, useBeforeRender } from "react-babylonjs";
+import {
+  ILoadedModel,
+  Model,
+  useBeforeRender,
+  useScene,
+} from "react-babylonjs";
 
 const Hero = () => {
+  const scene = useScene();
+  function onModelLoaded(model: ILoadedModel) {
+    console.log("model loaded: ", model);
+    console.log(typeof model);
+    if (model instanceof AbstractMesh) {
+      const animationGroups = model.animationGroups
+        ? model.animationGroups[0]
+        : null;
+      console.log("model animation: ", animationGroups);
+
+      const totalFrames = model.animationGroups
+        ? model.animationGroups[0]!.to
+        : 0;
+      console.log("total frames: ", totalFrames);
+      console.log("onModelLoaded: ", model);
+    }
+
+    if (scene) {
+      // scene.animationGroups[0]?.stop();
+    }
+  }
+
   return (
-    <Suspense>
+    <Suspense fallback={<box name="fallback" />}>
       <box
         name="hero"
         width={1}
@@ -16,14 +48,18 @@ const Hero = () => {
         position={new Vector3(0, 1.1, 0)}
       >
         <Model
-          rootUrl={"https://models.readyplayer.me/"}
-          sceneFilename={"661a1b5d8106a27608ba6b5f.glb"}
+          // rootUrl={"https://models.readyplayer.me/"}
+          // sceneFilename={"661a1b5d8106a27608ba6b5f.glb"}
+          rootUrl={"/assets/"}
+          sceneFilename={"scene.gltf"}
           name={"661a1b5d8106a27608ba6b5f"}
           position={new Vector3(0, -1.1, 0)}
           isPickable={false}
+          // scaleToDimension={1}
           visibility={0}
           receiveShadows
           checkCollisions
+          onModelLoaded={onModelLoaded}
           // onLoadProgress={({ loaded, total }) => {
           //   console.log("loading hero", loaded, total);
           // }}
